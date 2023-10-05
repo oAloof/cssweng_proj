b962c312d581725db5b6c10e9665c0f7abc480c7
Aloof <97267353+oAloof@users.noreply.github.com>
2023-10-05 16:50:47 +0800

    Added the product model

---
+++ backend/models/productModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+// Product schema
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        // required: true // could be required?
+        default: 'Generic' // comment out if required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    images: {
+        type: Array,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number, 
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Product', productSchema)
