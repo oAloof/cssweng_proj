9aa3f4686537969f4ace0c32ca7be138db81f356
Aloof <97267353+oAloof@users.noreply.github.com>
2023-11-10 16:59:29 +0800

    Setting up with MERN stack

--- src/routes/adminOrdersRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const orderController = require('../controllers/adminOrdersController')
-
-// GET all orders
-router.get('/', orderController.allOrdersView)
-
-module.exports = router
