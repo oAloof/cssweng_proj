1112118630ccb3d1bbf4c204134b912e1bb8407c
Aloof <97267353+oAloof@users.noreply.github.com>
2023-11-11 23:41:38 +0800

    Setup frontend with vite

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import '../styles/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+        {' '}
+        <b>{count}</b>
+      </p>
+    </>
+  )
+}
+
+export default App
