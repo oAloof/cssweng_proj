509ad4e91132fdf678721b0c1edd92c8593739d8
Aloof <97267353+oAloof@users.noreply.github.com>
2023-11-12 21:17:56 +0800

    Working on login

--- frontend/src/views/login.jsx
+++ frontend/src/views/login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
 
         <div className={styles.mainLoginCont}>
             <img src="/logos/bxappliances_longLogoColor.png" alt="BX Appliances Logo" className={styles.bxLogo} />
-            <div className={styles.headingDiv}>
+            <div className={styles.headingDiv}> 
                 <p className={styles.loginText}>Log In</p>
                 <p className={styles.welcomeText}>Welcome back!</p>
             </div>
