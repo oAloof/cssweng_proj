f6b83d2f1650311c338358e65ab53fe38ed03342
Juhlia <juhliaplu@gmail.com>
2023-11-17 05:10:53 +0800

    installed tailwind

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import ReactDOM from "react-dom";
 import { RegistrationProvider } from "../contexts/RegistrationContext";
+import "../index.css";
 
 // STYLES
 import styles from "../styles/global.modules.css";
