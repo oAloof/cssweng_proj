95c70552b3e8c66c95a1ba4c71d3dd5b99232099
Juhlia <juhliaplu@gmail.com>
2023-11-20 21:36:45 +0800

    Sections now retrieve categories

--- frontend/src/views/customer/productsPage.jsx
+++ frontend/src/views/customer/productsPage.jsx
@@ -6,7 +6,7 @@ import Section from "../../components/customer/Section.jsx";
 
 const ProductsPage = () => {
   const [saleData, setSaleData] = useState(null);
-  const [ProductsListed, setProductsListed] = useState(false);
+  const [ProductsListed, setProductsListed] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,10 +41,10 @@ const ProductsPage = () => {
         <div className="mt-[7vh] pb-[15vh]">
           <Countdown saleData={saleData} />
           <section className="overflow-auto ">
-            <Section category="Big Discounts!" />
-            <Section category="Big Discounts!" />
-            <Section category="Big Discounts!" />
-            <Section category="Big Discounts!" />
+            <Section title="Big Discounts!" category="Energy Efficient" />
+            <Section title="Big Discounts!" category="Energy Efficient" />
+            <Section title="Big Discounts!" category="Energy Efficient" />
+            <Section title="Big Discounts!" category="Energy Efficient" />
           </section>
         </div>
       ) : (
@@ -60,21 +60,22 @@ export default ProductsPage;
 
 const NoProductsView = ({ saleData }) => {
   return (
-    <div className="flex min-w-screen min-h-screen  bg-slate-400 gap-4">
-      <div className="mt-[7vh] pb-[15vh] flex flex-col items-center justify-start">
-        <div className="w-full">
-          <Countdown saleData={saleData} />
-        </div>
-        <div className="justify-center items-center w-3/4 ">
-          <div className="object-contain text-center font-Nunito text-[#5c698c]  ">
-            {/* <h1 className="mb-4 font-bold">No sales at the moment!</h1> */}
-            <img src="/NoProducts.png" className="opacity-90 px-60" />
-            <h3 className="mt-4 font-bold">
-              We're busy curating the best deals for you.
-            </h3>
-            <p>Check back soon for exciting offers and future bodega sales!</p>
-          </div>
-        </div>
+    <div className="flex min-w-screen h-screen items-center justify-center bg-slate-400">
+      <div className="w-full absolute top-0 mt-[7vh]">
+        <Countdown saleData={saleData} />
+      </div>
+      <div className="flex flex-col items-center justify-center p-4 font-Nunito">
+        <img
+          src="/NoProducts.png"
+          alt="No products available"
+          className="opacity-90 max-w-xs md:max-w-md"
+        />
+        <h3 className="mt-4 font-bold text-[#5c698c]">
+          We're busy curating the best deals for you.
+        </h3>
+        <p className="text-[#5c698c]">
+          Check back soon for exciting offers and future bodega sales!
+        </p>
       </div>
     </div>
   );
