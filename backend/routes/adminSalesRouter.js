2528a705eb416320dc450fd77d6272bf849d5434
jacyong <90311432+kurochiii@users.noreply.github.com>
2023-11-20 20:44:06 +0800

    sales stuffs

--- backend/routes/adminSalesRouter.js
+++ backend/routes/adminSalesRouter.js
@@ -3,12 +3,26 @@ const express = require('express')
 const router = express.Router()
 
 const salesController = require('../controllers/adminSalesController')
+const { authenticate, isAdmin } = require('../middlewares/authenticate')
 
 // GET all sales
-router.get('/', salesController.allSalesView)
-router.get('/add', salesController.addSaleView)
-router.post('/add', salesController.addNewSale)
-router.post('/add/product', salesController.addNewSale)
+router.get('/getSales', salesController.getSales) //authenticate, isAdmin,
+
+
+// POST a new product
+router.post('/addSales', salesController.addSales)        
+
+// // DELETE a product
+// router.delete('/delete/:id', productController.deleteProduct)
+
+// // GET a single product
+// router.get('/:id', productController.singleProductView)
+
+// // GET the update product page
+// router.get('/edit/:id', productController.updateProductView)
+
+// // UPDATE a product
+// router.patch('/edit/:id', productController.updateProduct) 
 
 // export the router
 module.exports = router
